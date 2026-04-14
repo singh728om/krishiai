0f83d6d81d3bc11be8411ee71916b35d6e88537f
Shashibhushan Singh <pixom.ai@gmail.com>
2026-04-14 12:49:41 +0530

    Update config.ts
    API KEY

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9634370087-d843f",
   "appId": "1:47156594728:web:aa0c59c98b63573ba3cd1d",
-  "apiKey": "AIzaSyCxALRSaKFLnVE6vULhADK_s5Xm9HxadNE",
+  "apiKey": "Your_Gemini API KEY",
   "authDomain": "studio-9634370087-d843f.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "47156594728"
